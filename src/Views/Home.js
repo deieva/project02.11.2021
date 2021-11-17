3d1e7f281eb794c9eb3b8cce421c9366d22c2f80
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-17 09:57:18 +0200

    add carousel

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,4 +1,7 @@
 import '../Assets/Css/Home.css';
+import pic1 from '../Assets/Images/pic1.jpg';
+import pic2 from '../Assets/Images/pic2.jpg';
+import pic3 from '../Assets/Images/pic3.jpg';
 
 function Home() {
 
@@ -6,34 +9,34 @@ function Home() {
 
         <div>
             {/* <!-- Carousel --> */}
-            <div id="demo" class="carousel slide" data-bs-ride="carousel">
+            <div id="demo" className="carousel slide" data-bs-ride="carousel">
 
                 {/* <!-- Indicators/dots --> */}
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" className="active"></button>
                     <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
                     <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
                 </div>
 
                 {/* <!-- The slideshow/carousel --> */}
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="la.jpg" alt="Los Angeles" class="d-block" style={{width:'100%'}}/>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={pic1} alt="Los Angeles" className="d-block" style={{width:'100%'}}/>
                     </div>
-                    <div class="carousel-item">
-                        <img src="chicago.jpg" alt="Chicago" class="d-block" style={{width:'100%'}}/>
+                    <div className="carousel-item">
+                        <img src={pic2} alt="Chicago" className="d-block" style={{width:'100%'}}/>
                     </div>
-                    <div class="carousel-item">
-                        <img src="ny.jpg" alt="New York" class="d-block" style={{width:'100%'}}/>
+                    <div className="carousel-item">
+                        <img src={pic3} alt="New York" className="d-block" style={{width:'100%'}}/>
                     </div>
                 </div>
 
                 {/* <!-- Left and right controls/icons --> */}
-                <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon"></span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-                    <span class="carousel-control-next-icon"></span>
+                <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                    <span className="carousel-control-next-icon"></span>
                 </button>
             </div>
 
