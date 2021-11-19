3aafeb05a67f7a37b13f6a5722d89909e3cd37b0
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-19 13:41:11 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,10 +13,10 @@ function Header() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Gallery</a>
+                                <a className="nav-link" href="/gallery">Gallery</a>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
