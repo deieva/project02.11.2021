0cb5844ff0a3c75a357d2cbd94d616c790a74807
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-22 11:47:06 +0200

    Update Gallery.js

--- src/Views/Gallery.js
+++ src/Views/Gallery.js
@@ -1,11 +1,23 @@
 import '../Assets/Css/Gallery.css';
-import $ from 'jquery';
+// import $ from 'jquery';
 
 function Gallery() {
 
     return (
         <div>
-          
+            <body>
+                <div class="box">
+                    <span style={{i: '1'}}><img src="https://images.unsplash.com/photo-1534119768988-c496213eff76?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" /></span>
+                    <span style={{i: '2'}}><img src="https://images.unsplash.com/photo-1575880918403-f578c9078302?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" /></span>
+                    <span style={{i: '3'}}><img src="https://images.unsplash.com/photo-1523983254932-c7e6571c9d60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1204&q=80" /></span>
+                    <span style={{i: '4'}}><img src="https://images.unsplash.com/photo-1519699047748-de8e457a634e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" /></span>
+                    <span style={{i: '5'}}><img src="https://images.unsplash.com/photo-1489667897015-bf7a9e45c284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1187&q=80" /></span>
+                    <span style={{i: '6'}}><img src="https://images.unsplash.com/photo-1526382925646-27b5eb86796e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" /></span>
+                    <span style={{i: '7'}}><img src="https://images.unsplash.com/photo-1534607287018-541c7d97748f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80" /></span>
+                    <span style={{i: '8'}}><img src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1594&q=80" /></span>
+                </div>
+            </body>
+
         </div>
     )
 }
