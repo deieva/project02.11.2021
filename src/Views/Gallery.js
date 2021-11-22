b94e2a497caa745920722528f776f1dd931af851
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-22 11:52:22 +0200

    update gallery

--- src/Views/Gallery.js
+++ src/Views/Gallery.js
@@ -6,7 +6,7 @@ function Gallery() {
     return (
         <div>
             <body>
-                <div class="box">
+                <div className="box">
                     <span style={{i: '1'}}><img src="https://images.unsplash.com/photo-1534119768988-c496213eff76?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" /></span>
                     <span style={{i: '2'}}><img src="https://images.unsplash.com/photo-1575880918403-f578c9078302?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" /></span>
                     <span style={{i: '3'}}><img src="https://images.unsplash.com/photo-1523983254932-c7e6571c9d60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1204&q=80" /></span>
