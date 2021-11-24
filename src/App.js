0acc1f4e16c4c480300e192c0868f2b8639ea180
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-24 09:50:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
 
                     <Route path="/" element={<Home />} />
                     <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/contacts" element={<Contacts />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
