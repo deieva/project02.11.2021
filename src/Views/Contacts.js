f947180fda81432316853903b0bfec8805219e2e
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-25 11:38:46 +0200

    update style for contacts

--- src/Views/Contacts.js
+++ src/Views/Contacts.js
@@ -3,23 +3,20 @@ import '../Assets/Css/Contacts.css';
 function Contacts() {
 
     return (
-
         <div>
             <div className="contact">
                 <div className="content-box">
                     <div className="left"></div>
                     <div className="right">
-                        <h1 className="contact-me"><strong>Contact Me</strong></h1>
-
+                        <h1 className="contact-me align-center"><strong>Contact Me</strong></h1>
                         <input type="text" className="inBar" placeholder="Your Name" />
                         <input type="text" className="inBar" placeholder="E-mail" />
                         <input type="tel" className="inBar" placeholder="Phone Number" />
                         <textarea type="text" className="inBar" placeholder="Enter Text"></textarea>
-                        <button className="btn">Submit</button>
+                        <button className="btn-contact">Submit</button>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
