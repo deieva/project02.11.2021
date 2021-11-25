99a4b8d5490adeb00d545a3808b93a028d96b953
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-25 11:31:51 +0200

    update style for contacts

--- src/Views/Contacts.js
+++ src/Views/Contacts.js
@@ -5,23 +5,23 @@ function Contacts() {
     return (
 
         <div>
-            <div className="container">
+            <div className="contact">
                 <div className="content-box">
                     <div className="left"></div>
                     <div className="right">
-                        <h1><strong>Contact Me</strong></h1>
+                        <h1 className="contact-me"><strong>Contact Me</strong></h1>
 
-                        <input type="text" className="inBar" placeholder="Your Name"/>
-                        <input type ="text" className ="inBar" placeholder="E-mail"/>
-                        <input type ="tel" className ="inBar" placeholder="Phone Number"/>
-                        <textarea type ="text" className ="inBar" placeholder="Enter Text"></textarea>
-                        <button className ="btn">Submit</button>
-                        </div>
-                        </div>
+                        <input type="text" className="inBar" placeholder="Your Name" />
+                        <input type="text" className="inBar" placeholder="E-mail" />
+                        <input type="tel" className="inBar" placeholder="Phone Number" />
+                        <textarea type="text" className="inBar" placeholder="Enter Text"></textarea>
+                        <button className="btn">Submit</button>
                     </div>
-
                 </div>
-                )
+            </div>
+
+        </div>
+    )
 }
 
-                export default Contacts;
+export default Contacts;
