0a70e455fd54ca9abf3df8863fc3f150d0a3bf60
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-25 11:24:34 +0200

    Update Contacts.js

--- src/Views/Contacts.js
+++ src/Views/Contacts.js
@@ -3,9 +3,23 @@ function Contacts() {
     return (
 
         <div>
+            <div class="container">
+                <div class="content-box">
+                    <div class="left"></div>
+                    <div class="right">
+                        <h1><strong>Contact Me</strong></h1>
 
-        </div>
-    )
+                        <input type="text" class="inBar" placeholder="Your Name"/>
+                        <input type ="text" class ="inBar" placeholder="E-mail"/>
+                        <input type ="tel" class ="inBar" placeholder="Phone Number"/>
+                        <textarea type ="text" class ="inBar" placeholder="Enter Text"></textarea>
+                        <button class ="btn">Submit</button>
+                        </div>
+                        </div>
+                    </div>
+
+                </div>
+                )
 }
 
-export default Contacts;
+                export default Contacts;
