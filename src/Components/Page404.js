8342886785573e05839b69bfd8a4d2ccd7f0969d
vaivodeieva <87878157+deieva@users.noreply.github.com>
2021-11-30 11:32:41 +0200

    Update Page404.js

--- src/Components/Page404.js
+++ src/Components/Page404.js
@@ -5,41 +5,41 @@ function Page404() {
     return (
 
         <div>
-            
-<div className="rail">
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-  <div className="stamp four">4</div>
-  <div className="stamp zero">0</div>
-</div>
-<div className="world">
-  <div className="forward">
-    <div className="box">
-      <div className="wall"></div>
-      <div className="wall"></div>
-      <div className="wall"></div>
-      <div className="wall"></div>
-      <div className="wall"></div>
-      <div className="wall"></div>
-    </div>
-  </div>
-</div>
+
+            <div className="rail">
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+                <div className="stamp four">4</div>
+                <div className="stamp zero">0</div>
+            </div>
+            <div className="world">
+                <div className="forward">
+                    <div className="box">
+                        <div className="wall"></div>
+                        <div className="wall"></div>
+                        <div className="wall"></div>
+                        <div className="wall"></div>
+                        <div className="wall"></div>
+                        <div className="wall"></div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
